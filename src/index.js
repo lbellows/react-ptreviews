4997da18d6614087a338e45f35c62b099bcd83a8
lbellows <lbellows@gmail.com>
2018-10-16 19:24:57 -0400

    update react-scripts

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import {Footer} from './StaticContent';
 import './css/slate.css';
 import './css/styles.css';
+import 'react-app-polyfill/ie11';
 
 console.log("index.js init");
 
